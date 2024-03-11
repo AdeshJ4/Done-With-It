6ec98147bae37b74b356212aa515198650beb8db
Adesh Jadhav <adejad1234@gmail.com>
2024-03-11 08:26:10 +0530

    Organizing Styles - create index file in each folder and from their export default your component

---
+++ app/components/AppText/AppText.jsx
@@ -0,0 +1,8 @@
+import { View, Text, StyleSheet, Platform } from "react-native";
+import styles from "./styles";
+
+const AppText = ({ children }) => {
+  return <Text style={styles.text}>{children}</Text>;
+};
+
+export default AppText;
