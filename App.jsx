efdec21d600e1e63d51098d7582a451158ada2f5
Adesh Jadhav <adejad1234@gmail.com>
2024-03-25 22:11:33 +0530

    Stack Navigator

--- App.jsx
+++ App.jsx
@@ -1,9 +1,38 @@
-import ListingEditScreen from './app/screens/ListingEditScreen'
+import { Text } from "react-native";
+import ScreenContainer from "./app/components/ScreenContainer";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tweets = () => {
+  return (
+    <ScreenContainer>
+      <Text>Tweet</Text>
+    </ScreenContainer>
+  );
+};
+
+const TweetDetails = () => {
+  return (
+    <ScreenContainer>
+      <Text>Tweet Details</Text>
+    </ScreenContainer>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator initialRouteName="Tweets">
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen name="Tweets" component={Tweets} />
+  </Stack.Navigator>
+);
 
 const App = () => {
   return (
-    <ListingEditScreen />
-  )
-}
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
 
-export default App
+export default App;
