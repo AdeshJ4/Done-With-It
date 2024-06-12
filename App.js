1d692bdd0788e79cb5e230daab6e73a97aae5966
Adesh Jadhav <adejad1234@gmail.com>
2024-06-12 23:55:32 +0530

    Learn about Text api
    useful methods:
    
    1. numberOfLines={5}
    2. onPress={onPressTitle}

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, StyleSheet, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [titleText, setTitleText] = useState("Bird's Nest");
+  const bodyText = "This is not really a bird nest.";
+
+  const onPressTitle = () => {
+    setTitleText("Bird's Nest [pressed]");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.baseText}>
+        <Text style={styles.titleText} onPress={onPressTitle}>
+          {titleText}
+          {"\n"}
+          {"\n"}
+        </Text>
+        <Text numberOfLines={5}>{bodyText}</Text>
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  baseText: {
+    fontFamily: "Cochin",
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
+
+export default App;
