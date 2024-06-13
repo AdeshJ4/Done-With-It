26909fa37f55b1e3701fc94a458dfae6b5acacf7
Adesh Jadhav <adejad1234@gmail.com>
2024-06-13 22:38:22 +0530

    Button with color

--- App.js
+++ App.js
@@ -3,7 +3,11 @@ import { View, Text, StyleSheet, Button } from "react-native";
 const App = () => {
   return (
     <View style={styles.container}>
-      <Button title="Click Me" onPress={() => console.log("Button Pressed")} />
+      <Button
+        title="Click Me"
+        color="orange"
+        onPress={() => console.log("Button Pressed")}
+      />
     </View>
   );
 };
