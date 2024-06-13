bcf8da62ebbdb349d313d5861f47f59c8734a283
Adesh Jadhav <adejad1234@gmail.com>
2024-06-13 22:50:47 +0530

    Alert.alert

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
-const { View, Text, StyleSheet, Button } = require("react-native");
+const { View, StyleSheet, Button, Alert } = require("react-native");
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Button title="Click Me" onPress={() => alert("Button Pressed")} />
+      <Button
+        title="Submit"
+        onPress={() => {
+          Alert.alert("Exit", "Do you really want to exit?", [
+            { text: "Yes", onPress: () => console.log("Yes") },
+            { text: "No", onPress: () => console.log("No") },
+          ]);
+        }}
+      />
     </View>
   );
 };
