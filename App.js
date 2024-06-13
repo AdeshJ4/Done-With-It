72782d1218a42ebf980646d64056763b778fd775
Adesh Jadhav <adejad1234@gmail.com>
2024-06-13 22:56:01 +0530

    Alert.prompt doesn't work with android

--- App.js
+++ App.js
@@ -3,15 +3,7 @@ const { View, StyleSheet, Button, Alert } = require("react-native");
 const App = () => {
   return (
     <View style={styles.container}>
-      <Button
-        title="Submit"
-        onPress={() => {
-          Alert.alert("Exit", "Do you really want to exit?", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ]);
-        }}
-      />
+      <Button title="Age" onPress={()=>Alert.prompt("Age", "Enter Age", (age)=>console.log(age))} />
     </View>
   );
 };
