984fe27224b9a860f82029f3ed3a9b274309829e
Adesh Jadhav <adejad1234@gmail.com>
2024-06-14 09:11:50 +0530

    One Style Object override the Style another object.

--- App.js
+++ App.js
@@ -1,24 +1,14 @@
-import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+const { View, StyleSheet, Button, Alert } = require("react-native");
 
 const App = () => {
-  const [titleText, setTitleText] = useState("Bird's Nest");
-  const bodyText = "This is not really a bird nest.";
-
-  const onPressTitle = () => {
-    setTitleText("Bird's Nest [pressed]");
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.baseText}>
-        <Text style={styles.titleText} onPress={onPressTitle}>
-          {titleText}
-          {"\n"}
-          {"\n"}
-        </Text>
-        <Text numberOfLines={5}>{bodyText}</Text>
-      </Text>
+    <View style={[styles.container, { backgroundColor: "dodgerblue" }]}>
+      <Button
+        title="Age"
+        onPress={() =>
+          Alert.prompt("Age", "Enter Age", (age) => console.log(age))
+        }
+      />
     </View>
   );
 };
@@ -28,13 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  baseText: {
-    fontFamily: "Cochin",
-  },
-  titleText: {
-    fontSize: 20,
-    fontWeight: "bold",
+    backgroundColor: "orange",
   },
 });
 
